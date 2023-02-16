b9778383d815a0e2f2c5ac72827aa07bc88bd00d
yunseonyeong <dbstjsdud99@gmail.com>
2023-02-16 17:28:32 +0900

    [FEAT] next config에 notion s3 domain 등록하기

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['https://s3.us-west-2.amazonaws.com'],
+    minimumCacheTTL: 60,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
