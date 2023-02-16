8030bbccf8c33257aa44c129130ea50a46e226a6
yunseonyeong <dbstjsdud99@gmail.com>
2023-02-17 00:08:30 +0900

    [FEAT] 코드블럭 컴포넌트

--- src/components/CodeBlockRenderer.tsx
+++ src/components/CodeBlockRenderer.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { dracula, materialLight, materialOceanic, vsDark, nightOwl, duotoneSea, duotoneForest, vs, synthwave84, pojoaque, xonokai, funky, ghcolors, vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 
 const CodeBlockRenderer = ({ code, language }: { code: any; language: any; }) => {
     return (
-        <SyntaxHighlighter
-            style={atomDark}
-            language={language}
-            PreTag="div">
-            {code.replace(/\n$/, "")}
-        </SyntaxHighlighter>
+        <Wrapper>
+            <SyntaxHighlighter
+                style={vscDarkPlus}
+                language={language}
+                PreTag="div">
+                {code.replace(/\n$/, "")}
+            </SyntaxHighlighter>
+        </Wrapper>
     );
 };
 
 export default CodeBlockRenderer;
+
+const Wrapper = styled.div`
+    @media (min-width:900px) {
+        max-width: 75%;
+    }
+    max-width: 90%;
+   border-radius: 4%;
+   overflow: hidden;
+`;
