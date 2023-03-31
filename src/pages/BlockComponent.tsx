1033678df585d354e7db8323c87f97f893254fd0
yunseonyeong <dbstjsdud99@gmail.com>
2023-03-31 19:07:50 +0900

    [FEAT] 태그별 Styled Component 생성

---
+++ src/pages/BlockComponent.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Header1 = styled.div`
+    font-size: 28px;
+    font-family: 'Noto Sans';
+    font-weight: 800;
+    padding: 8px 0;
+`;
+
+export const Header2 = styled.div`
+    font-size: 24px;
+    font-family: 'Noto Sans';
+    font-weight: 700;
+    padding: 8px 0;
+`;
+
+export const Header3 = styled.div`
+    font-size: 20px;
+    font-family: 'Noto Sans';
+    font-weight: 500;
+    padding: 8px 0;
+`;
+
+export const Divider = styled.hr`
+    border-top: 2px solid #bbb;
+`;
+
+export const PTag = styled.p`
+    padding-left: 10px;
+    font-size: 14.5px;
+    font-family: 'Noto Sans';
+`;
+
+export const NumberedItem = styled.div`
+    font-size: 16px;
+    font-family: 'Noto Sans';
+`;
+
+export const BulletedTag = styled.div`
+    font-size: 17px;
+    font-family: 'Noto Sans';
+    display: flex;
+    align-items: center;
+    gap: 2px;
+`;
+
+export const ImageDom = styled.div`
+    min-height: 400px;
+    min-width: 300px;
+    position: relative;
+    max-width: 85%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
